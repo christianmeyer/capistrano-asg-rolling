763cc7e5adef0803a14b52988485c4d797906984
Christian Meyer <github@rchristian.de>
2024-05-31 18:39:15 +0200

    refactored

--- lib/capistrano/asg/rolling/autoscale_group.rb
+++ lib/capistrano/asg/rolling/autoscale_group.rb
@@ -36,7 +36,7 @@ module Capistrano
         def launch_template
           @launch_template ||= begin
             template = aws_autoscaling_group.launch_template
-            template ||= aws_autoscaling_group.mixed_instances_policy.launch_template.launch_template_specification rescue nil
+            template ||= aws_autoscaling_group.mixed_instances_policy&.launch_template&.launch_template_specification
             raise Capistrano::ASG::Rolling::NoLaunchTemplate if template.nil?
 
             LaunchTemplate.new(template.launch_template_id, template.version, template.launch_template_name)
